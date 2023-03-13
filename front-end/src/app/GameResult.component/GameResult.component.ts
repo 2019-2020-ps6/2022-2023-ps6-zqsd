7ff8cda3442d9b230e010b0b025f0d5c0aa1e817
LorisDrid <loris.drid@etu.univ-cotedazur.fr>
2023-03-13 11:33:51 +0100

    GameResult

---
+++ front-end/src/app/GameResult.component/GameResult.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+@Component({
+    selector: 'app-game-result',
+    templateUrl: './GameResult.component.html',
+    styleUrls: ['./GameResult.component.scss']
+})
+export class MyEpicComponent implements OnInit {
+    constructor() {}
+    ngOnInit(): void {}
+} 
