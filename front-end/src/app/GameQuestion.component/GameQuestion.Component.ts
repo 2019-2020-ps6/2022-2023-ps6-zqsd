bcbf1bfad1baf44e9f2868bff67bc0fd9cea96c2
Swannben <swannbenz@gmail.com>
2023-03-20 11:58:19 +0100

    Refactor le concept de subquizz en Question

--- front-end/src/app/GameQuestion.component/GameQuestion.Component.ts
+++ front-end/src/app/GameQuestion.component/GameQuestion.Component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { GameService } from '../../services/GameService';
-import { Question } from '../../models/subQuiz.model';
-import { QuestionQuizz } from '../../mocks/subQuizz.mock';
+import { Question,Answer } from '../../models/Question.model';
+import { QuestionQuizz } from '../../mocks/question.mock';
 
 @Component({
   selector: 'app-game-question',
@@ -10,10 +10,10 @@ import { QuestionQuizz } from '../../mocks/subQuizz.mock';
 })
 export class GameQuestionComponent implements OnInit {
   
-  currentQuestion$: Question = {id:'', value: ''}; 
+  currentQuestion: Question = {id:'', value: '',label:"",answers: [] as Answer[]}; 
   constructor(private gameService: GameService) {
     this.gameService.AllQuestions$.subscribe((question: Question) => {
-      this.currentQuestion$ = question;
+      this.currentQuestion = question;
     });
   }
 
--- front-end/src/services/GameService.ts
+++ front-end/src/services/GameService.ts
@@ -4,8 +4,8 @@ import { HttpClient } from '@angular/common/http';
 import { serverUrl } from '../configs/server.config';
 import { QuizExample } from '../mocks/quizz.mock';
 import { Quiz } from '../models/quiz.model';
-import { Question } from 'src/models/subQuiz.model';
-import { QuestionQuizz } from 'src/mocks/subQuizz.mock';
+import { Question } from 'src/models/Question.model';
+import { QuestionQuizz } from 'src/mocks/question.mock';
 
 
 
