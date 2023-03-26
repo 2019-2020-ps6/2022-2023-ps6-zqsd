af6278ce8d98d1b48bd505808bb3da5421cc7b26
LorisDrid <loris.drid@etu.univ-cotedazur.fr>
2023-03-26 16:58:13 +0200

    correction #6

--- front-end/src/app/GameAnswer.component/GameAnswer.Component.ts
+++ front-end/src/app/GameAnswer.component/GameAnswer.Component.ts
@@ -11,28 +11,17 @@ import { Answer } from 'src/models/Question.model';
 export class GameAnswer_Component implements OnInit {
 
   currentAnswer$: Answer = {label:'', value:'', isCorrect: undefined, order: undefined};
-  score: number = 0; // Ajout d'une variable score initialisée à 0
 
   constructor(private gameService: GameService) {
     this.gameService.currentAnswer$.subscribe((answer: Answer) => {
       this.currentAnswer$ = answer;
-      if(this.getAnswerResult()){ // Si la réponse est correcte
-        this.score++; // On incrémente le score de 1
-      }
     });
+
   }
 
   getAnswerResult(){
     return this.currentAnswer$.isCorrect;
   }
-  getScore(){
-    return this.score;
-  }
     ngOnInit(): void {}
-
-    showScore() {
-      const score = this.getScore();
-      // Afficher le score dans une boîte de dialogue ou un composant dédié
-      }
 }
 
--- front-end/src/app/GameResult.component/GameResult.component.ts
+++ front-end/src/app/GameResult.component/GameResult.component.ts
@@ -1,19 +1,34 @@
 import { GameService } from './../../services/GameService';
 import { Component, OnInit, Output } from '@angular/core';
 import { AnswerClassic1 } from 'src/mocks/question.mock';
+
 @Component({
-    selector: 'app-game-result',
-    templateUrl: './GameResult.component.html',
-    styleUrls: ['./GameResult.component.scss']
+  selector: 'app-game-result',
+  templateUrl: './GameResult.component.html',
+  styleUrls: ['./GameResult.component.scss']
 })
 export class GameResultComponent implements OnInit {
-    score: number;
-    result: string;
+  score: number;
+  result: string;
 
-    constructor(private gameService: GameService) {
-      
-      this.score = 0;
-      this.result = '';
-    }
-    ngOnInit(): void {}
+  constructor(private gameService: GameService) {
+    this.score = 0;
+    this.result = '';
+  }
+
+  ngOnInit(): void {
+      this.gameService.answerResult$.subscribe((isCorrect: boolean|undefined) => {
+          if (isCorrect) {
+              this.score++;
+          }
+      });
+  }
+
+  getScore(): number {
+      return this.score;
+  }
+
+  endGame(): void {
+      this.result = 'Votre score est ' + this.score;
+  }
 }
