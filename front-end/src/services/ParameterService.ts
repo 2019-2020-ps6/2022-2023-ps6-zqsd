fbf4c8b5b038f799aba9ddd553d401057ac37fe7
guillaumeArrigoni <guillaume.arr@gmail.com>
2023-03-27 10:01:10 +0200

    S2 - Update ParameterService

--- front-end/src/services/ParameterService.ts
+++ front-end/src/services/ParameterService.ts
@@ -1,16 +1,7 @@
-import { Quiz1 } from './../mocks/quizz.mock';
-import { AnswerClassic1 } from './../mocks/question.mock';
-import { GameAnswerComponent } from './../app/GameAnswer.component/GameAnswer.Component';
-import { Injectable } from '@angular/core';
-import { BehaviorSubject, Subject, Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
-import { serverUrl } from '../configs/server.config';
-import { QuizExample } from '../mocks/quizz.mock';
-import { Quiz } from '../models/quiz.model';
-import { Question,Answer } from 'src/models/Question.model';
-import { QuestionQuizz } from 'src/mocks/question.mock';
 import { Parameter } from 'src/models/Parameter/parameter.model';
 import { PARAMETER } from 'src/mocks/Parameter/parameter.mock';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
@@ -33,25 +24,12 @@ import { PARAMETER } from 'src/mocks/Parameter/parameter.mock';
         return this.currentMusic$.asObservable();
     }
   
-    setCurrentQuestion(musicEnable: Parameter['music']) {
-      this.AllQuestions$.next(question);
+    setCurrentMusic(musicEnable: Parameter['music']) {
+      this.currentMusic$.next(musicEnable);
     }
-    getQuizList(): Quiz[] {
-      return this.quizList;
-    }
-    getCurrentAnswer(): Observable<any> {
-      return this.AllQuestions$.asObservable();
-    }
-  
-    add(quiz:Quiz) : void {
-      this.quizList.push(quiz)
-      this.quizList$.next(this.quizList)
-    }
-    getcurrentAnswer(): Observable<any> {
-      return this.currentAnswer$.asObservable();
-    }
-    getCurrentQuiz(): Observable<Quiz> {
-      return this.currentQuiz$.asObservable();
+
+    setCurrentSize(size: Parameter['size']) {
+        this.currentSize$.next(size);
     }
   
   }
