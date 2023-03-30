2ef9dbc87633c819282da6eee8dadf91a596a4df
guillaumeArrigoni <guillaume.arr@gmail.com>
2023-03-30 22:03:34 +0200

    S1 - Finish Memory Work in Advanced Parameter Finish Advanced Parameter ts files #9

--- front-end/src/app/AdvancedParamter/MemoryWork/MemoryWorkAdvancedParameter.component.ts
+++ front-end/src/app/AdvancedParamter/MemoryWork/MemoryWorkAdvancedParameter.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AdvancedParameterFocusWork, AdvancedParameterMemoryWork } from 'src/models/Parameter/advancedParameter.model';
+import { AdvancedParameterService } from 'src/services/Parameter/AdvancedParameterService';
 
 
 @Component({
@@ -8,6 +10,70 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class MemoryWorkAdvancedParameterComponent implements OnInit {
+    complexQuestionIsEnable : AdvancedParameterMemoryWork['complexQuestion'] = this.advancedParameterService.getCurrentComplexQuestion();
+    puzzleIsEnable : AdvancedParameterMemoryWork['puzzle'] = this.advancedParameterService.getCurrentPuzzle();
+    reflexionIsEnable : AdvancedParameterMemoryWork['reflection'] = this.advancedParameterService.getCurrentReflection();
+    memoryIsEnable : AdvancedParameterMemoryWork['memory'] = this.advancedParameterService.getCurrentMemory();
+    logicIsEnable : AdvancedParameterMemoryWork['logic'] = this.advancedParameterService.getCurrentLogic();
+
+    textDisplayComplexeQuestionButton : string = this.setTextDisplayButton(this.complexQuestionIsEnable);
+    textDisplayPuzzleButton : string = this.setTextDisplayButton(this.puzzleIsEnable);
+    textDisplayReflexionButton : string = this.setTextDisplayButton(this.reflexionIsEnable);
+    textDisplayMemoryButton : string = this.setTextDisplayButton(this.memoryIsEnable);
+    textDisplayLogicButton : string = this.setTextDisplayButton(this.logicIsEnable);
+
+
+    constructor(
+        private advancedParameterService: AdvancedParameterService) {
+        this.advancedParameterService.currentBackGround$.subscribe((complexQuestionIsEnable: AdvancedParameterMemoryWork['complexQuestion']) => {
+            this.complexQuestionIsEnable = complexQuestionIsEnable;
+            this.textDisplayComplexeQuestionButton = this.setTextDisplayButton(this.complexQuestionIsEnable)
+        });
+        this.advancedParameterService.currentQuestionAnimation$.subscribe((puzzleIsEnable: AdvancedParameterMemoryWork['puzzle']) => {
+            this.puzzleIsEnable = puzzleIsEnable;
+            this.textDisplayPuzzleButton = this.setTextDisplayButton(this.puzzleIsEnable)
+        });
+        this.advancedParameterService.currentRightAnswerAnimation$.subscribe((reflexionIsEnable: AdvancedParameterMemoryWork['reflection']) => {
+            this.reflexionIsEnable = reflexionIsEnable;
+            this.textDisplayReflexionButton = this.setTextDisplayButton(this.reflexionIsEnable)
+        });
+        this.advancedParameterService.currentWrongAnswerAnimation$.subscribe((memoryIsEnable: AdvancedParameterMemoryWork['memory']) => {
+            this.memoryIsEnable = memoryIsEnable;
+            this.textDisplayMemoryButton = this.setTextDisplayButton(this.memoryIsEnable)
+        });
+        this.advancedParameterService.currentWrongAnswerAnimation$.subscribe((logicIsEnable: AdvancedParameterMemoryWork['logic']) => {
+            this.logicIsEnable = logicIsEnable;
+            this.textDisplayLogicButton = this.setTextDisplayButton(this.logicIsEnable)
+        });
+    }
+
     ngOnInit(): void {
+    }
+
+    switchCurrentComplexQuestion(){
+        this.advancedParameterService.switchCurrentComplexQuestion()
+    }
+
+    switchCurrentPuzzle(){
+        this.advancedParameterService.switchCurrentPuzzle()
+    }
+
+    switchCurrentReflection(){
+        this.advancedParameterService.switchCurrentReflection()
+    }
+
+    switchCurrentMemory(){
+        this.advancedParameterService.switchCurrentMemory()
+    }
+
+    switchCurrentLogic(){
+        this.advancedParameterService.switchCurrentLogic()
+    }
+
+    setTextDisplayButton(isEnable : boolean) {
+        if (isEnable){
+            return "Activé" //We print the state of the button
+        }
+        return "Desactivé" //We print the state of the button
     }
 }
