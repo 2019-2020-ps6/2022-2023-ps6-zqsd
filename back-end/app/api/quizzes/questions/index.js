2071cf9acc16c3ac480b0eeb9f801386714e130e
RomainTho <romain.tho@etu.univ-cotedazur.fr>
2023-05-30 11:20:19 +0200

    fini question index

--- back-end/app/api/quizzes/questions/index.js
+++ back-end/app/api/quizzes/questions/index.js
@@ -26,3 +26,30 @@ router.get('/:questionId', (req, res) => {
     manageAllErrors(res, err)
   }
 })
+
+router.post('/', (req, res) => {
+  try {
+    const question = Question.create({ ...req.body })
+    res.status(201).json(quiz)
+  } catch (err) {
+    manageAllErrors(res, err)
+  }
+})
+
+router.put('/:questionId', (req, res) => {
+  try {
+    res.status(200).json(Question.update(req.params.questionId, req.body))
+  } catch (err) {
+    manageAllErrors(res, err)
+  }
+})
+
+router.delete('/:questionId', (req, res) => {
+  try {
+    Question.delete(req.params.questionId)
+    res.status(204).end()
+  } catch (err) {
+    manageAllErrors(res, err)
+  }
+})
+
