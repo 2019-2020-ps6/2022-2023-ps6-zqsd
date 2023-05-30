c971033adf909d150c1bca6ed9ddec14d6225c7b
Swannben <swannbenz@gmail.com>
2023-05-30 10:08:45 +0200

    quizz maintenant récupère les questions à partir de la liste d'id qu'il a

---
+++ back-end/app/api/quizzes/manager.js
@@ -0,0 +1,28 @@
+const { Quiz } = require('../../models')
+const { filterQuestionsFromQuizz } = require('./questions/manager')
+const { filterAnswersFromQuestion } = require('./questions/answers/manager')
+
+/**
+ * Function buildQuizz.
+ * This function aggregates the questions and answers from the database to build a quizz with all the data needed by the clients.
+ * @param quizId
+ */
+const buildQuizz = (quizId) => {
+  const quiz = Quiz.getById(quizId)
+  const questions = filterQuestionsFromQuizz(quiz.questions)
+  return { ...quiz, questions: questions }
+}
+
+/**
+ * Function buildQuizzes.
+ * This function aggregates the questions and answers from the database to build entire quizzes.
+ */
+const buildQuizzes = () => {
+  const quizzes = Quiz.get()
+  return quizzes.map((quiz) => buildQuizz(quiz.id))
+}
+
+module.exports = {
+  buildQuizz,
+  buildQuizzes,
+}
