6035f9dcca5cf62caeb85b8fe333b8db6a93d326
RomainTho <romain.tho@etu.univ-cotedazur.fr>
2023-06-12 11:51:18 +0200

    fix playwright setting

--- front-end/e2e/Scénarios/homepage-test.spec.ts
+++ front-end/e2e/Scénarios/homepage-test.spec.ts
@@ -2,14 +2,6 @@ import { test, expect } from '@playwright/test';
 import {homepage, testURL} from 'e2e/e2e.config';
 
 test.describe('Homepage test', () => {
-  test('Basic test', async ({page}) => {
-    await page.goto(homepage);
-    // Let's try with something you don't have in your page.
-    const pageTitle = await page.getByRole('heading', {name: 'AGreatHeadingNameYouDontHave'});
-    // It should not be visible as you don't have it in your page.
-    expect(pageTitle).not.toBeVisible();
-    // Test case pass? Means the playwright setup is done! Congrats!
-    });
     test('Test 1', async ({page}) => {
       await page.goto(homepage);
       await page.click('button.btn-jouer');
