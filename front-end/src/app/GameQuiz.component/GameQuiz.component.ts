642e11a7fda5d583ad398d4811c5d64802ab0574
RomainTho <romain.tho@etu.univ-cotedazur.fr>
2023-06-12 15:29:35 +0200

    fix classic route résultats

--- front-end/src/app/GameQuiz.component/GameQuiz.component.ts
+++ front-end/src/app/GameQuiz.component/GameQuiz.component.ts
@@ -91,15 +91,15 @@ export class GameQuizComponent {
 
   //on go next, pas de badAnswer donc pas de ++ a faire
   getNextQuestion(x:boolean){
-    if (this.isFinnished) {
+    console.log(this.gameService.allQuestionsAnswered() + " allQuestionsAnswered");
+    this.gameService.nextQuestion();
+    this.setTimer();
+    if (this.gameService.allQuestionsAnswered()) {
       this.router.navigateByUrl('/results');
       this.questionCounter = 1;
       this.isFinnished = false;
       console.log("zzzzzzzzzzzzzzzz")
     }
-    this.gameService.nextQuestion();
-    this.setTimer();
-
   }
 
   ngOnInit(){
